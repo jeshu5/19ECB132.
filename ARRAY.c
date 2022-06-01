84303753292b9015290d2e588de1d7b5b30922f7
jeshu5 <101857413+jeshu5@users.noreply.github.com>
2022-06-01 04:42:48 +0000

    output

--- ARRAY.c
+++ ARRAY.c
@@ -77,6 +77,7 @@ int main()
     }
     return 0;
 }
-output
+/*output
 gcc ARRAY.c
 Enter 5 elementEnter 6 elementEnter 7 elementEnter 8 elementEnter 9 elementEnter 10 elementEnter 11 elementEnter 12 elementEnter 13 elementEnter 14 elementEnter 15 elementEnter 16 elementEnter 17 elementEnter 18 elementEnter 19 elementEnter 20 elementEnter 21 elementEnter 22 elementEnter 23 elementEnter 24 elementEnter 25 elementEnter 26 elementEnter 27 elementEnter 28 elementEnter 29 elementEnter 30 elementEnter 31 elementEnter 32 elementEnter 33 elementEnter 34 elementEnter 35 elementEnter 36 elementEnter 37 elementEnter 38 elementEnter 39 elementEnter 40 elementEnter 41 elementEnter 42 elementEnter 43 elementEnter 44 elementEnter 45 elementEnter 46 elementEnter 47 elementEnter 48 elementEnter 49 elementEnter 50 elementEnter 51 elementEnter 52 elementEnter 53 elementEnter 54 elementEnter 55 elementEnter i to insert/ d to deleteWrong choice~/19ECB132$ 
+  */
